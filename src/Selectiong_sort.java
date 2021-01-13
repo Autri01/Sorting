14ca4268cbc47e467c60a360b52db6547d5e2877
Autri <autrininja@gmail.com>
2021-01-13 18:06:52 +0700

    first

---
+++ src/Selectiong_sort.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Selectiong_sort {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        System.out.println("Enter the number of elements of the list");
+        int n=scn.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=scn.nextInt();
+        }
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
